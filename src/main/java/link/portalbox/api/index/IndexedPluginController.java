43f6f52964d38ec24febd2b570f4f08be7670c03
sttephen <stephenatcollege@Gmail.com>
2023-01-25 05:58:33 +0000

    - Added error response

--- src/main/java/link/portalbox/api/index/IndexedPluginController.java
+++ src/main/java/link/portalbox/api/index/IndexedPluginController.java
@@ -1,4 +1,23 @@
 package link.portalbox.api.index;
 
+import com.google.gson.Gson;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@CrossOrigin(origins = {"http://127.0.0.1:5173", "https://portalbox.link"}, maxAge = 3600)
+@RestController
+@RequestMapping(value = "/index", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class IndexedPluginController {
+
+    private HashMap<Integer, String> indexCache = new HashMap<>();
+    private Gson gson = new Gson();
+
+    @GetMapping
+    public ResponseEntity<Object> index() {
+        return ResponseEntity.accepted().body(gson.toJson(indexCache));
+    }
+
 }
--- src/main/java/link/portalbox/api/index/IndexedPluginRepository.java
+++ src/main/java/link/portalbox/api/index/IndexedPluginRepository.java
@@ -1,12 +1,9 @@
 package link.portalbox.api.index;
 
-import link.portalbox.api.plugin.Plugin;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-
 @Repository
-public interface IndexedPluginRepository extends MongoRepository<IndexedPlugin, Integer> {
+public interface IndexedPluginRepository extends MongoRepository<IndexedPluginController, Integer> {
 
 }
